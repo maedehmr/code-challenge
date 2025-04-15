59cb4618d535e2a6d75116c68075ba733592afee
maedehmr <maedehmrjb@gmail.com>
2025-04-15 11:57:04 +0330

    feat: work on home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { CommentSection } from "./_components";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.title}>
+        <Image src="/icons/comment.svg" width={24} height={24} alt="comment" />
+        <h3>نظرات من</h3>
+        <CommentSection />
+      </div>
+    </main>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Vazirmatn } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const vazir = Vazirmatn({
+  variable: "--font-vazir",
 });
 
 export const metadata: Metadata = {
@@ -23,10 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+    <html lang="fa" dir="rtl">
+      <body className={`${vazir.variable}`}>{children}</body>
     </html>
   );
 }
